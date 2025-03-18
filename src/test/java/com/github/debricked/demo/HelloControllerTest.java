8c5fe30ef4e1a1aa586d7b744d8f309d9e45df5a
orachid <94545032+orachid@users.noreply.github.com>
2025-03-18 11:17:59 +0100

    Update HelloControllerTest.java

--- src/test/java/com/github/debricked/demo/HelloControllerTest.java
+++ src/test/java/com/github/debricked/demo/HelloControllerTest.java
@@ -19,7 +19,7 @@ public class HelloControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
-
+//test action
 	@Test
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
